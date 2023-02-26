58f6081cfa228850741be7ff461f9887395006db
Tihomir Trifonov <ttrifonovmain@gmail.com>
2023-02-26 12:10:52 +0200

    Update app.js

--- JavaScript-Advanced/Advanced Functions - Exercise/09. Task Manager/app.js
+++ JavaScript-Advanced/Advanced Functions - Exercise/09. Task Manager/app.js
@@ -6,11 +6,10 @@ function solve() {
     document.querySelectorAll("section");
 
   const addButton = document.getElementById("add");
-  addButton.addEventListener("click", addTask);
+  addButton.addEventListener("click", addToDo);
 
-  function addTask(event) {
+  function addToDo(event) {
     event.preventDefault();
-    /*
     if (
       taskField.value.trim() === "" ||
       descriptionField.value.trim() === "" ||
@@ -22,6 +21,7 @@ function solve() {
     console.log("not refresh");
 
     const newTask = document.createElement("article");
+
     const header = document.createElement("h3");
     header.textContent = taskField.value;
     newTask.appendChild(header);
@@ -31,18 +31,43 @@ function solve() {
     const paragraphDate = document.createElement("p");
     paragraphDate.textContent = dateField.value;
     newTask.appendChild(paragraphDate);
+
     const buttons = document.createElement("div");
     newTask.appendChild(buttons);
     const startButton = document.createElement("button");
     startButton.textContent = "Start";
-    startButton.classList("green");
+    startButton.classList.add("green");
+    startButton.addEventListener("click", moveToInProgress);
     buttons.appendChild(startButton);
     const deleteButton = document.createElement("button");
-    deleteButton.textContent = "Start";
-    deleteButton.classList("red");
+    deleteButton.textContent = "Delete";
+    deleteButton.classList.add("red");
+    deleteButton.addEventListener("click", deleteTask);
     buttons.appendChild(deleteButton);
 
     open.appendChild(newTask);
-    */
+
+    taskField.value = "";
+    descriptionField.value = "";
+    dateField.value = "";
+
+    function moveToInProgress() {
+      startButton.remove();
+      const finishButton = document.createElement("button");
+      finishButton.textContent = "Finish";
+      finishButton.classList.add("orange");
+      finishButton.addEventListener("click", moveToComplete);
+      buttons.appendChild(finishButton);
+      inProgress.appendChild(newTask);
+    }
+
+    function deleteTask() {
+      newTask.remove();
+    }
+
+    function moveToComplete() {
+      buttons.remove();
+      complete.appendChild(newTask);
+    }
   }
 }
